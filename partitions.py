f4684bc68e592129ece49f8b328ca5613a601386
Alex Campbell <acampb191@gmail.com>
2024-01-07 21:33:01 -0500

    oops didn't save a file

--- partitions.py
+++ partitions.py
@@ -2,14 +2,14 @@ import pandas as pd
 import datetime
 
 def partition_file():
-    file = pd.read_csv("funnel.csv")
+    file = pd.read_csv("loans.csv")
 
-    file = file.sort_values(['action_date', 'checkout_id', 'action'])
+    file = file.sort_values(['checkout_date', 'merchant_id'])
 
-    file['month'] = pd.to_datetime(file['action_date']).dt.strftime('%m%Y')
+    file['month'] = pd.to_datetime(file['checkout_date'], format="mixed").dt.strftime('%m%Y')
 
     distinct_months = file['month'].drop_duplicates()
 
     for month_name in distinct_months:
         new_file = file.loc[file['month'] == month_name]
-        new_file.to_csv("funnel_" + month_name + ".csv") 
+        new_file.to_csv("loans_" + month_name + ".csv") 
